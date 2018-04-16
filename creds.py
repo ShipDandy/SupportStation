fd89e38d03e32d521289d0db6ffdbd37cd0f949a
joe brower <zyoosun@gmail.com>
2018-04-16 12:06:57 -0500

    cred cover up

--- creds.py
+++ creds.py
@@ -1,13 +1,37 @@
-zdc_headers = {"Authorization": "Bearer jH0cBAutzUiJ3r5HjaouQeqivWjffhJdxHJ6pKth1vMYzYPfWoiR40OWGDge9pKI", "Content-Type": "application/json", "Accept": "Application/Json"}
 
-zd_headers = {"Authorization": "Basic am9lLmJyb3dlckBzaGlwc3RhdGlvbi5jb206WmVicmEwMFNT", "Content-Type": "application/json", "Accept": "Application/Json"}
-
-pvCreds = {"host": "secure-files.playvox.com", "port": 2222, "username": "shipstation", "pass": "Sh1pst4t1onPlvx", "folder": "files"}
-
-sgCreds = {"username": "SendGridBackend@ShipStation.com", "key": "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"}
-
-drpCreds = {
-    "Authorization": "Bearer 8amnoeZVAtAAAAAAAAAABpa8PyxA5o5-RTLWk8mEzy4JJ3LKCD5RQ6Adge0lNNJu",
-    "Content-Type": "application/octet-stream",
-    "Dropbox-API-Arg": ""
-}
+                                                          >*
+                                                   #      >*
+  " UDDER FOOLISHNESS "                            #  ###>***~~~~~|
+                                                   ####  *****^^^#
+                                              _____|       *#####
+                                             | ^^^#   \/ \/ #
+                                            ##^^###         |
+                                             ### ##*        *
+ |_                                ********~~|_____>         *
+ \\|_                 ________************        #>>***    ***
+ \\\\|_             __|     *************        ## >>>*  *****
+ |___  |______   __|         ***********       ##>### ^^^^^^^^^^
+    |____    |__|           **********       >>>>## ^<^^^^^@^^^^^
+         #          ***      ********      **>>>># ^<^^@^^^@^^^^^
+          #      ***********    ******     *>>>## ^<<^^^^^^^^<<<
+          #      ***********    ******    **>>>## ^<<<<^^^<<<<<
+         #        *********      ****   ***>>>#### ^<<<<<<<<<
+         #         **********          ****>>>###### <<<<<
+         ##        **********          ****>>>>##      ##
+         ##         **  ***             ****>>>>        #     ##XXX
+         ##**                            *******         ##>>>>#XX
+          >>*                             ******         #######XXX
+          >>*****                           ***         ##__
+           >>*****   **** ***               **    *****     \__
+           >># **    *********              *********>>>#      XXX
+           ##        *********              *******>>>>>##     XXX
+        |~~           ********                 *>>>>> >#######XXX
+    X~~~~ ###          *********          ######>          >>>XXXX
+  XXX  #>>>##          ********>>##  #######
+   XXX#>      #   ##>>>>>>>>>>>>>###UUUUU^^
+   XXX        #  ####>>>>>>>>>>UUUUUUUUU^^
+              #  >>           UUUUUU^^^<()
+             #  >              U()^<()  ()
+           *#  *>               ()  ()
+          **** #
+            ***
